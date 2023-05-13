cdb20199471c20e98d70c7771e8df300fd37487c
wetyuiopsu <51488420+wetyuiopsu@users.noreply.github.com>
2023-05-13 16:00:35 -0500

    ya

---
+++ src/main/java/frc/robot/subsystems/Arm/Pivot.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.Arm;
+
+public class Pivot {
+    
+}
--- src/main/java/frc/robot/subsystems/Arm/Telescope.java
+++ src/main/java/frc/robot/subsystems/Arm/Telescope.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.Arm;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
